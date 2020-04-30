5f680c057d55b38689944ee136c397472b54712e
Khurram Shehzad <khurram.pak522@gmail.com>
2020-04-30 16:08:57 +0500

    Do not use return statement in single line method body

--- NetworkClient/NetworkClient/Internal/Parser/JsonParser.swift
+++ NetworkClient/NetworkClient/Internal/Parser/JsonParser.swift
@@ -11,6 +11,6 @@ import Foundation
 final class JsonParser: Parser {
   
 func parse(_ data: Data) throws -> MoviesResponse {
-  return try JSONDecoder().decode(MoviesResponse.self, from: data)
+  try JSONDecoder().decode(MoviesResponse.self, from: data)
 }
 }
